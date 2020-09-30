f7743269006e7662f479879795e2b27bf16a07e5
Husnah-The-Programmer <67584836+Husnah-The-Programmer@users.noreply.github.com>
2020-09-30 17:37:08 +0100

    Add files via upload

---
+++ conditional statement.py
@@ -0,0 +1,34 @@
+# A python program to find the numbers of days in month  
+# input number from user 
+print ("month number:1-12") 
+month_number = input("enter the month number:")
+if month_number== "1":
+    print("31 days")
+elif month_number== "2":
+    print("28 or 29 days")
+elif month_number== "3":
+    print("31 days")
+elif month_number== "4":
+    print("30 days")
+elif month_number=="5":
+    print("31 days")
+elif month_number=="6":
+    print17("30 days")
+elif month_number=="7":
+    print("31 days")
+elif month_number=="8":
+    print("31 days")
+elif month_number=="9":
+    print("30 days")
+elif month_number=="10":
+    print("31 days")
+elif month_number=="11":
+    print("30 days")
+elif month_number=="12":
+    print("31 days")
+else:
+    print("wrong input!please enter month number within the range of (1-12)")
+
+
+    
+
